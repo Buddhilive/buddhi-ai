db7d3d17321a27db8ab8b6ccf19bc28d9de098da
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-10-08 22:03:11 +0530

    Add basic layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,28 @@
+"use client";
 import Image from "next/image";
 
 export default function Home() {
+  const downloadModel = async () => {
+    // Check for user activation.
+    if (navigator.userActivation.isActive) {
+      console.log(await (window as any).LanguageModel.availability());
+
+      const session = await (window as any).LanguageModel.create({
+        monitor(m: any) {
+          m.addEventListener("downloadprogress", (e: any) => {
+            console.log(`Downloaded ${e.loaded * 100}%`);
+          });
+        },
+      });
+
+      // Prompt the model and stream the result:
+      const stream = session.promptStreaming("Write me an extra-long poem!");
+      for await (const chunk of stream) {
+        console.log(chunk);
+      }
+    }
+  };
+
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
@@ -23,6 +45,12 @@ export default function Home() {
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
           </li>
+          <button
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+            onClick={downloadModel}
+          >
+            Download
+          </button>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
