42428f68e3f21075b116b05ec080432871f050e0
Buddhi Kavindra <visitbudkavin@gmail.com>
2026-04-04 21:40:23 +0530

    Remove old project

--- src/app/(app)/knowledge/page.tsx
+++
@@ -1,5 +0,0 @@
-import { DocumentsView } from "@/components/custom/documents-view";
-
-export default function DocumentsPage() {
-    return <DocumentsView />;
-}
--- src/app/(app)/models/page.tsx
+++
@@ -1,5 +0,0 @@
-import { ModelsView } from "@/components/custom/model-view";
-
-export default function ModelsPage() {
-    return <ModelsView />;
-}
