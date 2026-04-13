64154ccdacc4ff87714fe54a4be7555774266927
Buddhi Kavindra <visitbudkavin@gmail.com>
2026-04-13 10:53:52 +0530

    Add Branding images

--- src/components/team-switcher.tsx
+++ src/components/team-switcher.tsx
@@ -39,7 +39,7 @@ export function TeamSwitcher({
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
               <div className="text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
-                <Image src="/icons/favicon-32x32.png" alt="Buddhi AI Logo" width={32} height={32} />
+                <Image src="/icons/icon-48x48.png" alt="Buddhi AI Logo" width={32} height={32} />
               </div>
               <div className="grid flex-1 text-left text-sm leading-tight">
                 <span className="truncate font-medium">{activeTeam.name}</span>
